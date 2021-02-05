e5e0a06218b2f15f23313e2da527f4a72ea9557b
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-02-05 13:58:00 +0000

    Add configuration + more - Move asm package to dev.amber.audioswitcher

---
+++ src/main/java/dev/amber/audioswitcher/asm/impl/LibraryLWJGLOpenALImpl.java
@@ -0,0 +1,36 @@
+package dev.amber.audioswitcher.asm.impl;
+
+import dev.amber.audioswitcher.AudioSwitcher;
+import dev.amber.audioswitcher.config.AudioSwitcherConfig;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.openal.AL;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LibraryLWJGLOpenALImpl {
+    public static void createAL() throws LWJGLException {
+        try {
+            // Create an OpenAL context for searching for devices
+            AL.create();
+
+            // Get all devices
+            AudioSwitcher.getInstance().getDevices();
+            List<String> devices = AudioSwitcher.getInstance().devices;
+
+            // Destroy the old context
+            AL.destroy();
+
+            // Create an OpenAL instance with our sound device, or if it doesn't exist just use the default
+            if(devices.contains(AudioSwitcherConfig.SELECTED_SOUND_DEVICE)) {
+                AL.create(AudioSwitcherConfig.SELECTED_SOUND_DEVICE, 44100, 60, false);
+            } else {
+                // Fallback to default sound device, selected one isn't available
+                AudioSwitcher.getInstance().logger.warn("Failed to find previously selected sound device, using system default...");
+                AL.create();
+            }
+        } catch (Exception e) {
+            AL.create();
+        }
+    }
+}
---
+++ src/main/java/dev/amber/audioswitcher/config/AudioSwitcherConfig.java
@@ -0,0 +1,32 @@
+package dev.amber.audioswitcher.config;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.client.FMLClientHandler;
+
+import java.io.File;
+
+public class AudioSwitcherConfig {
+    private static final File configFile = new File(FMLClientHandler.instance().getClient().mcDataDir, "config/audioswitcher.config");
+
+    public static String SELECTED_SOUND_DEVICE = "";
+
+    public static void saveConfig() {
+        Configuration configuration = new Configuration(configFile);
+        updateConfig(configuration, false);
+        configuration.save();
+    }
+
+    public static void loadConfig() {
+        Configuration configuration = new Configuration(configFile);
+        configuration.load();
+        updateConfig(configuration, true);
+    }
+
+    private static void updateConfig(Configuration configuration, boolean load) {
+        Property selectedSoundDeviceProperty = configuration.get("General", "selectedSoundDevice", "");
+
+        if (load) SELECTED_SOUND_DEVICE = selectedSoundDeviceProperty.getString();
+        else selectedSoundDeviceProperty.setValue(SELECTED_SOUND_DEVICE);
+    }
+}
