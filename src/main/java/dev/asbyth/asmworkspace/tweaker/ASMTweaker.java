05f3353e88cced6568b1946aad0dbefa71c628da
Amber <71222289+dreamhopping@users.noreply.github.com>
2021-02-05 12:13:00 +0000

    Initial commit

---
+++ src/main/java/dev/asbyth/asmworkspace/tweaker/ASMTweaker.java
@@ -0,0 +1,36 @@
+package dev.asbyth.asmworkspace.tweaker;
+
+import net.minecraftforge.common.ForgeVersion;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+// forge warns when this annotation is missing so make it shut up
+@IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
+public class ASMTweaker implements IFMLLoadingPlugin {
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[]{ClassTransformer.class.getName()};
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
